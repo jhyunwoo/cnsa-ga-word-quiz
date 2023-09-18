76fd8c2621eb9ff8211268b42874b0679c2d32cb
jhyunwoo <jhyunwoo0228@gmail.com>
2023-09-18 18:08:06 +0900

    add metadata and robots.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,10 @@ import Loading from "@/components/Loading";
 export const metadata: Metadata = {
   title: "이뿌영 퀴즈",
   description: "이미지로 뿌리 뽑는 영단어 퀴즈",
+  openGraph: {
+    title: "이뿌영 퀴즈",
+    description: "이미지로 뿌리 뽑는 영단어 퀴즈",
+  },
 };
 
 export default function RootLayout({
--- app/profile/history/layout.tsx
+++ app/profile/history/layout.tsx
@@ -1,24 +1,25 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { Metadata } from "next";
 import { getServerSession } from "next-auth";
+import { ReactNode } from "react";
 import { redirect } from "next/navigation";
-import type { ReactNode } from "react";
+import type { Metadata } from "next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export const metadata: Metadata = {
-  title: "이뿌영 퀴즈 | 테스트",
+  title: "이뿌영 퀴즈 | 테스트 기록",
   description: "이미지로 뿌리 뽑는 영단어 퀴즈",
   openGraph: {
-    title: "이뿌영 퀴즈 | 테스트",
+    title: "이뿌영 퀴즈 | 테스트 기록",
     description: "이미지로 뿌리 뽑는 영단어 퀴즈",
   },
 };
 
-export default async function TestLayout({
+export default async function ProfileLayout({
   children,
 }: {
   children: ReactNode;
 }) {
   const session = await getServerSession(authOptions);
+
   if (!session) redirect("/auth/signin");
 
   return <>{children}</>;
